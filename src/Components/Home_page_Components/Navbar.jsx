c51b4a374934a65e7e4822f1e63fc567153e6d6e
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-02-19 10:53:09 -0600

    initial commit, Nav-bar(Done)

---
+++ src/Components/Home_page_Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import logo from '../../Images/Logo.png'
+import '../../CSS/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <header className="Nav-bar">
+      <img src={logo} className="school-name " />
+      <button type="button" className="Login-btn">
+        Login <span class="material-symbols-outlined">login</span>
+      </button>
+    </header>
+  )
+}
+export default Navbar
