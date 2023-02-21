0506c17c58db24ae91d65900e318a3ae5cbdfd94
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-02-20 19:30:04 -0600

    added descriptions,made a nav bar fixed to top

--- Frontend/src/Components/Home_page_Components/classes.jsx
+++ Frontend/src/Components/Home_page_Components/classes.jsx
@@ -1,22 +1,42 @@
 import { classes } from './HomeData'
+import { useState } from 'react'
 import '../../CSS/classes.css'
 
 const Classes = () => {
+  // const [showMore, setShowMore] = useState('')
+
+  // const changeDetails = () => {
+  //   setShowMore('Extra-details')
+  // }
+
   return (
     <div className="classes">
-      {classes.map(({ id, name, sem, postion, Notes, Course_Description }) => {
+      {classes.map(({ id, name, sem, position, Notes, Course_Description }) => {
         return (
           <>
-            <label key={id} className="class">
-              <>
+            <label
+              key={id}
+              className="class-container"
+              // onClick={() => setShowMore(id)}
+            >
+              <div className="class">
                 <p className="class-name">
                   <strong>{name}</strong>
                 </p>
                 <p className="class-sem">
                   (<strong>{sem}</strong>)
                 </p>
-              </>
-              <div className="Extra-details">details</div>
+              </div>
+
+              {/* {showMore == id && ( */}
+              <div className="Extra-details">
+                <p className="position">Position: {position}</p>
+                <p className="Notes">Notes: {Notes}</p>
+                <a href={Course_Description} target="_blank">
+                  Course Description
+                </a>
+              </div>
+              {/* )} */}
             </label>
           </>
         )
