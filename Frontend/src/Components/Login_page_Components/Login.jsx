09b87d8d38d45d506e60bcda2349915ad20b1049
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-03-04 20:03:21 -0600

    added the link between title and homepage

--- Frontend/src/Components/Login_page_Components/Login.jsx
+++ Frontend/src/Components/Login_page_Components/Login.jsx
@@ -1,82 +1,85 @@
-import { useRef, useState, useEffect } from 'react';
-import "./../../CSS/loginPage.css"
+import { useRef, useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './../../CSS/loginPage.css'
 const Login = () => {
-    const userRef = useRef();
-    const errRef = useRef();
+  const userRef = useRef()
+  const errRef = useRef()
 
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+  const [user, setUser] = useState('')
+  const [pwd, setPwd] = useState('')
+  const [errMsg, setErrMsg] = useState('')
+  const [success, setSuccess] = useState(false)
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+  useEffect(() => {
+    setErrMsg('')
+  }, [user, pwd])
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(user, pwd);
-        setUser('');
-        setPwd('');
-        setSuccess(true);
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(user, pwd)
+    setUser('')
+    setPwd('')
+    setSuccess(true)
+  }
 
+  useEffect(() => console.log('logging here'), [])
 
+  return (
+    <>
+      <h2>lOGIN sCREEN TEST</h2>
+      {success ? (
+        <div className="centered-aligned-sections">
+          <section>
+            <h1> Welcome! </h1>
+            <br />
+            <p>
+              <Link to="/">
+                <strong>Return to Homepage</strong>
+              </Link>
+            </p>
+          </section>
+        </div>
+      ) : (
+        <div className="centered-aligned-sections">
+          <section>
+            <p
+              ref={errRef}
+              className={errMsg ? 'errmsg' : 'offscreen'}
+              aria-live="assertive"
+            >
+              {' '}
+              {errMsg}{' '}
+            </p>
 
-
-    useEffect(() => console.log("logging here"), [])
-
-    return (
-        <>
-            <h2>lOGIN sCREEN TEST</h2>
-            {success ? (
-                <div className='centered-aligned-sections'>
-                    <section>
-                        <h1> Welcome! </h1>
-                        <br />
-                        <p>
-                            <a href="#" > Go to Homepage </a>
-                        </p>
-                    </section>
-                </div>
-
-            ) : (
-                <div className='centered-aligned-sections'>
-                    <section>
-                        <p ref={errRef} className={errMsg ? "errmsg" :
-                            "offscreen"} aria-live="assertive" > {errMsg} </p>
-
-                        <h1> Login In </h1>
-                        <form onSubmit={handleSubmit}>
-                            <label htmlFor="username" > Username:</label>
-                            <input
-                                type="text"
-                                id="username"
-                                ref={userRef}
-                                autoComplete="off"
-                                onChange={(e) => setUser(e.target.value)}
-                                value={user}
-                                required
-                            />
-                            <label htmlFor="password" > Password:</label>
-                            <input
-                                type="password"
-                                id="password"
-                                onChange={(e) => setPwd(e.target.value)}
-                                value={pwd}
-                                required
-                            />
-                            <button> Log In </button>
-                        </form>
-
-
-                    </section>
-                </div>
-            )}
-        </>
-    )
+            <h1> Login In </h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username"> Username:</label>
+              <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+              />
+              <label htmlFor="password"> Password:</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
+              <button> Log In </button>
+            </form>
+          </section>
+        </div>
+      )}
+    </>
+  )
 }
 export default Login
