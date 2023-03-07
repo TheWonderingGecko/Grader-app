66ea5bc782f9faf62b171816dd8031cc123168d4
mswvgc <98501566+mswvgc@users.noreply.github.com>
2023-03-06 18:33:04 -0600

    Add files via upload

---
+++ Backend/model/UserV2.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const userV2Schema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  studentId: {
+    type: Number,
+    required: true
+  },
+  undergradDegree: {
+    type: String,
+    required: false
+  },
+  gpa: {
+    type: Number,
+    required: true
+  },
+  hoursCompleted: {
+    type: Number,
+    required: true
+  },
+  graduatingSemester: {
+    type: String,
+    enum: ['fall23', 'spring23', 'summer23',
+          'fall24', 'spring24', 'summer24',
+          'fall25', 'spring25', 'summer25',
+          'fall26', 'spring26', 'summer26']
+  },
+  major: {
+    type: String,
+    enum: ['cs', 'ece', 'it'],
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ['undergraduate', 'graduate', 'doctoral']
+  },
+  applySemester: {
+    type: String,
+    enum: ['fall', 'spring', 'summer']
+  },
+  gtaCertified: {
+    type: String,
+    enum: ['yes', 'no', 'not interested'],
+    required: false
+  },
+  resumeFilePath: {
+    type: String,
+    required: false
+  },
+  transcriptFilePath: {
+    type: String,
+    required: false
+  }
+});
+
+export default mongoose.model('UserV2', userV2Schema);
+
