55dc1757f6d9a9f04172b815b39fd5217ffb26a4
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-04-04 12:22:18 -0500

    admin page started nav bar is somewhat responsive

--- Frontend/src/Components/Global_components/Navbar.jsx
+++ Frontend/src/Components/Global_components/Navbar.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <header className="Nav-bar">
-      <div className="school_department">
-        <img src={logo} className="school-name " />
-        <p className="department">Science and Engineering</p>
+    <header className=" bg-umkc_light_blue flex justify-between sticky top-0 items-center border-b-8 border-umkc_yellow">
+      <div className="flex flex-col  items-baseline pl-4 text-white  ">
+        <img src={logo} className="w-20 md:w-80" />
+        <p className="md:text-6xl lg:9xl md:pl-6 ">Science and Engineering</p>
       </div>
       <Link to="/login">
-        <button type="button" className="btn login-btn">
+        <button
+          type="button"
+          className="btn login-btn w-32 text-sm mr-4 h-10 font-semibold md:w-auto md:h-fit md:text-3xl"
+        >
           {' '}
           <strong>APPLY NOW</strong>
         </button>
