0eea804f29871fb1d859c76117435ac9507d6417
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-04-16 07:38:04 -0500

    added applicantion arrays for each course

--- Backend/model/Course.js
+++ Backend/model/Course.js
@@ -1,6 +1,56 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const applicationSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  studentID: {
+    type: String,
+    required: true,
+  },
+  umkcEmail: {
+    type: String,
+    required: true,
+  },
+  degree: {
+    type: String,
+    required: true,
+  },
+  gpa: {
+    type: Number,
+    required: true,
+  },
+  major: {
+    type: String,
+    enum: ['cs', 'ece', 'it'],
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ['BS', 'MS', 'PHD'],
+    required: true,
+  },
+  semester: {
+    type: String,
+    enum: ['Fall', 'Spring', 'Summer'],
+    required: true,
+  },
+  isGTA: {
+    type: Boolean,
+    required: true,
+  },
+  resumeFile: {
+    type: String,
+    required: true,
+  },
+})
+
 const courseSchema = new Schema({
   code: {
     type: String,
@@ -34,6 +84,8 @@ const courseSchema = new Schema({
     enum: ['undergraduate', 'graduate'],
     required: false,
   },
+
+  applications: [applicationSchema],
 })
 
 module.exports = mongoose.model('Course', courseSchema)
