e93aaa20435d4c4f915122c65d480f043c394122
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-04-17 13:42:47 -0500

    remove unneeded value

--- Frontend/src/Components/Admin_page_components/Position_card.jsx
+++ Frontend/src/Components/Admin_page_components/Position_card.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useCourseDeletion } from '../../hooks/useCourseDeletion'
 import { useCourseEdit } from '../../hooks/useCourseEdit'
-import { useApplication } from '../../hooks/useApplication'
 
 const Position_card = (props) => {
   const [edit, setEdit] = useState('')
