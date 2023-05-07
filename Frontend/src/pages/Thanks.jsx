2f9c03bdf20d9fc4a9748dd199f37342be251018
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-05-07 04:16:13 -0500

    added fade to log out page

--- Frontend/src/pages/Thanks.jsx
+++ Frontend/src/pages/Thanks.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 
 const Thanks = () => {
   return (
-    <div className="  flex items-center justify-center w-screen h-screen bg-[url('../assets/Big_roo.png')] bg-[20%,60%] lg:bg-[10%,60%]">
+    <div className="  flex items-center justify-center w-screen h-screen bg-[url('../assets/Big_roo.png')] bg-[20%,60%] lg:bg-[10%,60%] fade-in">
       <div className="w-3/4 p-4 text-center bg-white border-2 shadow-2xl border-umkc_light_blue">
-        <p className="font-semibold">
+        <p className="font-semibold ">
           Thank you for your submissions!! We will review you application(s) and
           contact you if we feel you're application is a good fit for the
           postion.
