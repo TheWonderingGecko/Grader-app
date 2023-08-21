783e673d612fbe357454694934e5d8a55b1b5eb0
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-08-21 07:35:22 -0500

    added comments for the backend file upload section

--- Backend/controllers/file-controller.js
+++ Backend/controllers/file-controller.js
@@ -1,12 +1,14 @@
-const multer = require('multer')
-const path = require('path')
+// Import the required modules
+const multer = require('multer') // Library for handling file uploads
+const path = require('path') // Node.js module for handling file paths
 
-// Configure multer storage
+// Configure multer storage settings
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, 'uploads/') // Set the directory where uploaded files will be stored
   },
   filename: (req, file, cb) => {
+    // Generate a unique filename for each uploaded file
     cb(
       null,
       file.fieldname + '-' + Date.now() + path.extname(file.originalname)
@@ -14,26 +16,30 @@ const storage = multer.diskStorage({
   },
 })
 
+// Create a multer middleware instance with the configured storage
 const upload = multer({ storage: storage }).fields([
-  { name: 'resume', maxCount: 1 },
-  { name: 'gtaCertification', maxCount: 1 },
+  // Define fields and their maximum upload count
+  { name: 'resume', maxCount: 1 }, // Field name is 'resume', allowing 1 file
+  { name: 'gtaCertification', maxCount: 1 }, // Field name is 'gtaCertification', allowing 1 file
 ])
 
 // File upload function
 const uploadFiles = (req, res) => {
   upload(req, res, (err) => {
     if (err) {
+      // Handle errors during file upload
       return res.status(500).json({ message: 'Error uploading files.' })
     }
 
     // If successful, send the file paths in the response
     res.json({
-      resume: req.files.resume[0].path,
+      resume: req.files.resume[0].path, // Path of the uploaded resume file
       gtaCertification: req.files.gtaCertification
-        ? req.files.gtaCertification[0].path
-        : null,
+        ? req.files.gtaCertification[0].path // Path of the uploaded GTA certification file, if uploaded
+        : null, // If no GTA certification file uploaded, set as null
     })
   })
 }
 
+// Export the uploadFiles function for use in other modules
 module.exports = { uploadFiles }
